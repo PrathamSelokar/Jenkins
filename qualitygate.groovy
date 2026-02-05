3b82c706cd370cc5b712c1fe0ca2a45d41c0f0a8
PrathamSelokar <154890106+PrathamSelokar@users.noreply.github.com>
2026-02-05 13:33:07 +0530

    Add files via upload

---
+++ qualitygate.groovy
@@ -0,0 +1,53 @@
+pipeline {
+    agent { label 'slave' }  
+
+    stages {
+
+        stage('git-pull') {
+            steps {
+                    git branch: 'main', url: 'https://github.com/Anilbamnote/student-ui-app.git'
+            }
+        }
+
+        stage('Build') {
+            steps {
+                sh '/opt/maven/bin/mvn clean package'
+            }
+        }
+
+        stage('Test') {
+            steps {
+                sh '/opt/maven/bin/mvn test'
+            }
+        }
+
+        stage('Quality_gate') {
+            steps {
+
+               timeout(10) {
+               echo "wait for at list 10 min to perform next action"
+            }
+
+               waitForQualityGate true
+            }
+        }
+
+        stage('SonarQube Analysis') {
+            steps {
+                withSonarQubeEnv('sonar-server') {
+                    sh '''/opt/maven/bin/mvn sonar:sonar \\
+                          -Dsonar.projectKey=my-student1 \\
+                          -Dsonar.host.url=http://13.251.81.239:9000 \\
+                          -Dsonar.login=fc33af1c728a70dcf2db398dda2a61649acc3f58'''
+                }
+            }
+        }
+
+        stage('Deploy') {
+            steps {
+                echo "deploy success"
+            }
+        }
+    }
+}
+
